1a96b2cd37c38ce65745306a87fc07d0a60cd2ce
Thualani Sikhosana <sikhosana@thedigitalacademy.co.za>
2021-08-31 11:16:06 +0200

    yeah

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const express = require("express");
-
-//product
-const product = require("/api/product");
-
-
+const product = require("./api/product");
 const app = express();
-
 const path = require("path");
 const logger = require("morgan");
 const cors = require("cors");
-
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
